f34a06984edf38fa4f8dbb15429c6985b4930b7d
Jhon Jhon <jj482@rambler.ru>
2020-06-28 16:33:15 +0300

    Проверена функция вычисления объема параллелипипида

--- FigureClass/src/Figure.java
+++ FigureClass/src/Figure.java
@@ -1,8 +1,9 @@
 public class Figure {
-    public double volume_parallelepiped(double x, double y, double z){
+    public static double volume_parallelepiped(double x, double y, double z){
         return x*y*z;
     }
     public static void main(String[] args) {
+        System.out.println(volume_parallelepiped(3, 4, 5));
 
     }
 }
