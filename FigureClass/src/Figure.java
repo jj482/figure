850de6f0ee051ecb36405741eabd23e83bfafadd
Jhon Jhon <jj482@rambler.ru>
2020-06-28 15:47:51 +0300

    Добавлена функция вычисления длины окружности

--- FigureClass/src/Figure.java
+++ FigureClass/src/Figure.java
@@ -1,4 +1,11 @@
 public class Figure {
+    public double len_circle(double radius){
+        double ret = 0;
+        if (radius>=0)
+            ret = 2*3.1415*radius;
+        return ret;
+    }
+
     public static void main(String[] args) {
 
     }
