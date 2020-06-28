3af955b05d78f5eea2b81927e21faa134daa37e1
Jhon Jhon <jj482@rambler.ru>
2020-06-28 16:51:49 +0300

    Сделаны static функции Проверены все функции

--- FigureClass/src/Figure.java
+++ FigureClass/src/Figure.java
@@ -1,16 +1,16 @@
 public class Figure {
 
-    public double len_circle(double radius){
+    public static double len_circle(double radius){
         double ret = 0;
         if (radius>=0)
             ret = 2*3.1415*radius;
         return ret;
     }
-    public double square_circle(double radius){
+    public static double square_circle(double radius){
         return 3.1415*radius*radius;
     }
 
-    public double perimeter_rectangle (double len, double wid){
+    public static double perimeter_rectangle (double len, double wid){
         return (2*len+2*wid);
     }
     public static double volume_parallelepiped(double x, double y, double z){
@@ -20,5 +20,8 @@ public class Figure {
     public static void main(String[] args) {
         System.out.println(volume_parallelepiped(3, 4, 5));
 
+        System.out.println(len_circle(3));
+        System.out.println(square_circle(1));
+        System.out.println(perimeter_rectangle(3,5));
     }
 }
